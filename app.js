6302d36ddba029a08c712d7debdba2102de43530
Kevin Allred <16504353+TheAllred@users.noreply.github.com>
2023-07-11 21:03:45 -0600

    something strange is happening

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.get("/", (req, res) => {
   if (req.oidc.isAuthenticated()) {
     res.redirect("https://pokedex-uifd.onrender.com");
   } else {
-    res.redirect("https://pokedex-uifd.onrender.com");
+    res.redirect("/");
   }
   // res.send(req.oidc.isAuthenticated() ? "Logged in" : "Logged out");
 });
