3eeeecad99958774e148d2f75cf321f6d18570ee
Kevin Allred <16504353+TheAllred@users.noreply.github.com>
2023-07-12 13:05:32 -0600

    add is authenticatedcheck to new bench

--- routes/benches.js
+++ routes/benches.js
@@ -7,7 +7,7 @@ router.get("/", benchController.getAll);
 router.get("/:id", benchController.getById);
 router.get("/byOwner/:email", benchController.getBenchByOwner);
 
-router.post("/new", benchController.createNew);
+router.post("/new", requiresAuth(), benchController.createNew);
 
 router.put("/update/:id", benchController.updateBench);
 
